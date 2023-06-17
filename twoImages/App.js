fb4ece09cd7935c1a0af4576b918920272ef1d91
James <39015355+nojameshere@users.noreply.github.com>
2023-06-17 13:12:21 -0600

    Completed the twoImages assignment
    This one had a lot of weird requirements. The touch Buttons

--- twoImages/App.js
+++ twoImages/App.js
@@ -1,15 +1,44 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useRef, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Animated, TouchableHighlight } from 'react-native';
 
 export default function App() {
+  const fadeInValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    const fadeIn = () => {
+      Animated.timing(fadeInValue, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start();
+    };
+
+    fadeIn();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Image 
-        source={{
-          height: 200,
-          width: 300,
-          uri: 'https://picsum.photos/id/5/200/300' //This is a blocked website on the network at my work.
-        }}
-      />
+      <View style={styles.imageContainer}>
+        <TouchableOpacity onPress={() => alert('You clicked on the school image')} opacity={0.5}>
+          <Animated.Image
+            source={require('./assets/dtc.jpg')}
+            style={[styles.localImage, { opacity: fadeInValue }]}
+          />
+        </TouchableOpacity>
+        <Text style={styles.text}>Davis Technical College</Text>
+      </View>
+      <View style={styles.container}>
+        <TouchableHighlight onPress={ () => alert('You booped the cat snoot')} underlayColor={'yellow'}>
+            <Image
+              source={{
+                height: 200,
+                width: 300,
+                uri: 'https://picsum.photos/id/40/200/300'
+              }}
+            />
+        </TouchableHighlight>
+        <Text style={styles.text}>Cat Nose</Text>
+      </View>
     </View>
   );
 }
@@ -17,8 +46,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: 'lightgreen',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  localImage: {
+    marginTop: 190,
+    width: 300,
+    height:200,
+    resizeMode: 'contain',
+    margin: 0
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  opacity: {
+
+  },
+  highlight: {
+    backgroundColor: '#FFFFFF',
+  }
 });
